3f4498628498594d099bb327a703fdfbb20eea9b
Giorgio-g97 <giorgio.g97@gmail.com>
2024-07-01 22:07:45 +0200

    add animations to Projects

--- portfolio_giorgio/src/common/ProjectCard.jsx
+++ portfolio_giorgio/src/common/ProjectCard.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import styles from "../sections/Projects/Projects.module.css"
+import {motion} from "framer-motion"
 
-const ProjectCard = ({src, link, h3}) => {
+const ProjectCard = ({src, link, liveLink, h3, isInView}) => {
+
+  const projectItem = {
+    start: { y: 20, opacity: 0 },
+    end: { y: isInView ? 0 : 20, opacity: isInView ? 1 : 0, transition: { duration: 0.3 }}
+  }
+
   return (
     <>
-        <a href={link} className={styles.containerLink} target='_blank'>
+        <motion.a
+          href={link}
+          className={styles.containerLink} target='_blank'
+          variants={projectItem}
+        >
           <img src={src} alt={`${h3} logo`} />
           <h3>{h3}</h3>
-          <p>Streaming App</p> {/* mettere icona */}
-          <p>Github Code</p> {/* mettere icona */}
-        </a>
+          <div>
+            <motion.a
+            href={liveLink}
+            initial={{scale: 1}}
+            whileHover={{scale: 1.05}}
+            whileTap={{scale: 0.95}}
+            transition={{duration: 0.3}}
+            >Streaming App</motion.a> {/* mettere icona */}
+            <motion.a 
+            href={link}
+            initial={{scale: 1}}
+            whileHover={{scale: 1.05}}
+            whileTap={{scale: 0.95}}
+            transition={{duration: 0.3}}
+            >Github Code</motion.a> {/* mettere icona */}
+          </div>
+        </motion.a>
     </>
   )
 }
--- portfolio_giorgio/src/sections/Projects/Projects.jsx
+++ portfolio_giorgio/src/sections/Projects/Projects.jsx
@@ -1,26 +1,55 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from "./Projects.module.css"
 import TodoList from "../../assets/project-1.png"
 import BilingsSite from "../../assets/project-2.png"
 import ProjectCard from '../../common/ProjectCard'
+import {motion, useInView} from "framer-motion"
 
 const Projects = () => {
+
+  const ref = useRef();
+  const isInView = useInView(ref);
+
+  const projectsContainer = {
+    start: {
+      scale: 0,
+      y: 20
+    },
+    end: {
+      scale: 1,
+      y: isInView ? 0 : 20,
+      transition: {
+        delayChildren: isInView ? 0.3 : 0,
+        staggerChildren: isInView ? 0.1 : 0,
+      }
+    }
+  }
   return (
     <>
         <section id='projects' className={styles.container}>
             <h1 className='sectionTitle'>Projects</h1>
-            <div className={styles.projectsContainer}>
-                <ProjectCard 
+            <motion.div 
+              className={styles.projectsContainer}
+              ref={ref}
+              variants={projectsContainer}
+              initial="start"
+              animate="end"  
+            >
+                <ProjectCard
+                    isInView={isInView} 
                     src={TodoList}
                     link="https://github.com/Giorgio-g97/todolist-react"
+                    liveLink="https://todolist-react-giorgio.netlify.app/"
                     h3="TodoList App"
                 />
                 <ProjectCard
+                  isInView={isInView}
                   src={BilingsSite}
+                  liveLink="https://todolist-react-giorgio.netlify.app/"
                   link="https://github.com/Giorgio-g97/billings-project"
                   h3="Billings Method Website"
                 />
-            </div>
+            </motion.div>
         </section>
     </>
   )
